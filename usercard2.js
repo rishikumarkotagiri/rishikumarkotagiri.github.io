ae25bd1f1c8c6c8ff1769d7f06f4755bf5085cdd
Rishi kumar <125175280+rishikumarkotagiri@users.noreply.github.com>
2023-06-15 19:23:54 +0530

    added new

---
+++ usercard2.js
@@ -0,0 +1,29 @@
+let bhuvioldinfo = {
+    imageurl: "https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2013/04/29/329127.gif?itok=SZN8Zu9e",
+    name: "bhuvi",
+    description: "sunrisers hyderabad cricketer"
+}
+
+let bhuviinfo={
+    imageurl: "https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2013/04/29/329127.gif?itok=SZN8Zu9e",
+    name: "Bhuvaneshwar Kumar",
+    description: "swing king of india"
+}
+
+let isbhuvioldinfo = true;
+let displayobject;
+let flipdata = function(){
+    if(isbhuvioldinfo==true){
+        displayobject = bhuviinfo;
+        isbhuvioldinfo = false;
+    }
+    else{
+        displayobject = bhuvioldinfo;
+        isbhuvioldinfo = true;
+    }
+    document.getElementById("bhuvi").src = displayobject.imageurl;
+    document.getElementById("bhuvaneshwarkumar").innerHTML = displayobject.name;
+    document.getElementById("descrip").innerHTML = displayobject.description;
+
+
+}
