11d37432168715cf71ce76460d77420ef97b0c31
Rishi kumar <125175280+rishikumarkotagiri@users.noreply.github.com>
2023-06-16 21:18:06 +0530

    added

--- usercard2.js
+++ usercard2.js
@@ -1,8 +1,8 @@
-let bhuvioldinfo = {
-    imageurl: "https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2013/04/29/329127.gif?itok=SZN8Zu9e",
-    name: "bhuvi",
-    description: "sunrisers hyderabad cricketer"
-}
+// let bhuvioldinfo = {
+//     imageurl: "https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2013/04/29/329127.gif?itok=SZN8Zu9e",
+//     name: "bhuvi",
+//     description: "sunrisers hyderabad cricketer"
+// }
 
 let bhuviinfo={
     imageurl: "https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2013/04/29/329127.gif?itok=SZN8Zu9e",
